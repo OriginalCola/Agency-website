a6993f05b0edb28b547f354a3d48326527942644
OriginalCola <142392135+OriginalCola@users.noreply.github.com>
2024-01-21 01:43:33 +0100

    Updates

--- components/LastWorks.tsx
+++ components/LastWorks.tsx
@@ -7,7 +7,7 @@ export default function Works() {
             id='lastest'
             className='mobile:flex justify-center pt-[10rem]'>
             <div className='desktop:ml-[10.625rem] tablet:ml-[5rem] mobile:ml-[1.5rem] z-50'>
-                <h1 className='text-[#fff] text-[2rem] font-[400] mt-[2rem]'>Our latest work</h1>
+                <h1 className='text-[#fff] text-[2rem] font-[400] mt-[2rem]'>Nowości</h1>
 
                 <div className='flex flex-row flex-wrap gap-[5rem] mt-[2rem]'>
                     <div className='w-[23rem] tablet:h-[24rem] mobile:h-[23rem]'>
@@ -18,8 +18,8 @@ export default function Works() {
                             height={250}
                             alt='Last work'
                         />
-                        <h1 className='text-[1.5625rem] text-[#fff] font-[300] mt-[1.5rem]'>Artem - Digital Marketing campaign</h1>
-                        <h3 className='text-[1.125rem] text-[#C9C9C9] font-[300] pb-[3rem]'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod</h3>
+                        <h1 className='text-[1.5625rem] text-[#fff] font-[300] mt-[1.5rem]'>Touchflow - coś niespotykanego</h1>
+                        <h3 className='text-[1.125rem] text-[#C9C9C9] font-[300] pb-[3rem]'>Niesamowity panel, który pomoże z codziennym zarządzaniem twoją stroną internetową.</h3>
                     </div>
                     <div className='w-[23rem] tablet:h-[24rem] mobile:h-[23rem]'>
                         <Image
@@ -40,8 +40,8 @@ export default function Works() {
                             height={250}
                             alt='Last work'
                         />
-                        <h1 className='text-[1.5625rem] text-[#fff] font-[300] mt-[1.5rem]'>Basic - Pay per click (PPC)</h1>
-                        <h3 className='text-[1.125rem] text-[#C9C9C9] font-[300]'>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod</h3>
+                        <h1 className='text-[1.5625rem] text-[#fff] font-[300] mt-[1.5rem]'>Trynityflow - <span className="text-transparent gradient-text animate-gradient">AI</span></h1>
+                        <h3 className='text-[1.125rem] text-[#C9C9C9] font-[300]'>Wyczerpująca wypowiedź na temat sztucznej inteligencji i użycia jej w celu ułatwienia działania przedsiębiorcom.</h3>
                     </div>
                 </div>
             </div>
--- components/Services.tsx
+++ components/Services.tsx
@@ -7,18 +7,18 @@ export default function Services() {
             id='services'
             className='pt-[5rem]'>
             <div className='grid desktop:grid-cols-4 gap-y-10 gap-x-5 desktop:mx-[10.625rem] desktop:mt-[8rem] tablet:mt-[4rem] mobile:mt-[1.5rem] mobile:grid-cols-2 mobile:mx-[2rem] tablet:mx-[5rem]'>
-                <div className={cardStyle}><img className={imgStyle} src="/icona1.png"></img><h1 className={textStyle}>Niesamowity wykres</h1></div>
-                <div className={cardStyle}><img className={imgStyle} src="/icon2.png"></img><h1 className={textStyle}>halo</h1></div>
+                <div className={cardStyle}><img className={imgStyle} src="/icona1.png"></img><h1 className={textStyle}>Analiza rynku i doposawanie trendom</h1></div>
+                <div className={cardStyle}><img className={imgStyle} src="/icon2.png"></img><h1 className={textStyle}>Tworzenie stron oraz aplikacji</h1></div>
                 <div className='col-span-2 desktop:h-[15rem] tablet:h-[12rem] mobile:h-[10rem] flex justify-center flex-col desktop:ml-[2rem] tablet:order-first mobile:order-first'>
                     <h1 className='text-[#FFF] desktop:text-[2.5rem] tablet:text-[2rem] mobile:text-[1.7rem] font-[500] pb-[1rem]'>Nasze metody zwiększania lukratywności</h1>
                     <h3 className='text-[#C9C9C9] desktop:text-[1.125rem] tablet:text-[1rem] mobile:text-[0.85rem] font-[400]'>Narzędzia które wykorzystują nasi eskperci w celu zwiększenia przychodowości oraz zainteresowania twojej działalnosci</h3>
                 </div>
             </div>
             <div className='grid desktop:grid-cols-4 gap-y-10 gap-x-5 desktop:mx-[10.625rem] mt-[2rem] tablet:grid-cols-2 mobile:grid-cols-2 tablet:mx-[5rem] mobile:mx-[2rem] '>
-                <div className={cardStyle}><img className={imgStyle} src="/icon3.png"></img><h1 className={textStyle}>halo</h1></div>
-                <div className={cardStyle}><img className={imgStyle} src="/icon4.png"></img><h1 className={textStyle}>halo</h1></div>
-                <div className={cardStyle}><img className={imgStyle} src="/icon5.png"></img><h1 className={textStyle}>halo</h1></div>
-                <div className={cardStyle}><img className={imgStyle} src="/icon6.png"></img><h1 className={textStyle}>halo</h1></div>
+                <div className={cardStyle}><img className={imgStyle} src="/icon3.png"></img><h1 className={textStyle}>Zwiększanie zainteresowania sztuką pisaną</h1></div>
+                <div className={cardStyle}><img className={imgStyle} src="/icon4.png"></img><h1 className={textStyle}>Treści umieszczane na platformach social media</h1></div>
+                <div className={cardStyle}><img className={imgStyle} src="/icon5.png"></img><h1 className={textStyle}>Chwylity marketing w social media </h1></div>
+                <div className={cardStyle}><img className={imgStyle} src="/icon6.png"></img><h1 className={textStyle}>Zwiększenie klikalności w Google</h1></div>
             </div>
         </section>
     );
